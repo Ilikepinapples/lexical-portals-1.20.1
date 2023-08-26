c39f6391bd6facb7dc99f5b12569cb54aeee797d
Ilikepinapples <willschofield08@gmail.com>
2023-08-26 19:17:02 +0100

    item

--- src/main/java/net/entity0/portal/LexicalPortalsClient.java
+++ src/main/java/net/entity0/portal/LexicalPortalsClient.java
@@ -1,10 +1,11 @@
 package net.entity0.portal;
 
+import net.entity0.portal.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class LexicalPortalsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/net/entity0/portal/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.entity0.portal.item;
+
+import net.entity0.portal.LexicalPortals;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+public class ModItems {
+    public static final Item UNOPENEDLEX = registerItem("unopened_book", new Item(new FabricItemSettings()));
+
+    private static void AddItemsToFunctionalItemGroup(FabricItemGroupEntries entries) {
+        entries.add(UNOPENEDLEX);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(LexicalPortals.Mod_Id, name), item);
+    }
+    public static void registerModItems () {
+        LexicalPortals.LOGGER.info("registering lexical items that portal for " + LexicalPortals.Mod_Id);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModItems::AddItemsToFunctionalItemGroup);
+    }
+}
