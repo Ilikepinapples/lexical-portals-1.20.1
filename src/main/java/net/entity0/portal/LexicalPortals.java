779da9520df516df1a30a91983b4ab0bb51b3595
Ilikepinapples <willschofield08@gmail.com>
2023-08-26 20:12:29 +0100

    item

--- src/main/java/net/entity0/portal/LexicalPortals.java
+++ src/main/java/net/entity0/portal/LexicalPortals.java
@@ -1,5 +1,6 @@
 package net.entity0.portal;
 
+import net.entity0.portal.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -17,7 +18,6 @@ public class LexicalPortals implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
