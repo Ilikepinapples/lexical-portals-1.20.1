25dae08e50128e82a63b623d502e70a7886238f2
Ilikepinapples <willschofield08@gmail.com>
2023-08-26 14:22:12 +0100

    inital commit

---
+++ src/main/java/net/entity0/portal/LexicalPortalsClient.java
@@ -0,0 +1,10 @@
+package net.entity0.portal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LexicalPortalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/entity0/portal/LexicalPortalsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.entity0.portal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LexicalPortalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
